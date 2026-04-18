4c0614ee1d5679b32455e8a9c0242fcc31de5820
Rubber Ducky Software Development <78836929+RDSD-Dev@users.noreply.github.com>
2026-04-18 05:31:35 -0600

    Charecter side movement
    Still needs work the jumping is jarring working on a fix.

--- Assets/Scripts/AltCharecterMovement.cs
+++ Assets/Scripts/AltCharecterMovement.cs
@@ -15,11 +15,10 @@ public class AltCharecterMovement : MonoBehaviour
     public float gravity = -16f;
     public float airAcc = 14f;
     public float maxAirVel = 42f;
-    public float airResistance = 2f;
     public float maxFallVel = -28f;
 
     [Header("Buffers")]
-    public float jumpBuffer = .066f; // Counts early jump presses
+    public float jumpBuffer = .1f; // Counts early jump presses
     private float jumpBufferTime = 0f;
 
     [Header("Setup")]
@@ -29,8 +28,8 @@ public class AltCharecterMovement : MonoBehaviour
 
     private float groundDist = .2f;
     private bool isGrounded;
+
     private Vector3 velocity;
-    private Vector3 lastDir;
 
     // Update is called once per frame
     void Update()
@@ -39,46 +38,52 @@ public class AltCharecterMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 currentDir = transform.right * x + transform.forward * z;
 
-        // Handle Jump
-        if(Input.GetButtonDown("Jump") || jumpBufferTime > 0)
+        Vector3 wishDir = Vector3.Normalize(currentDir);
+
+        if(isGrounded && velocity.y < 0)
+            velocity.y = 0;
+        else
         {
-            if (Physics.CheckSphere(groundCheck.position, groundDist, groundMask)) // We don't update isGrounded until after so bunny hopping is possible
-            {
-                lastDir = currentDir;
-                velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
-            }
-            else if(jumpBufferTime <= 0)
+            velocity.y += gravity * Time.fixedDeltaTime ;
+            Debug.Log(velocity.y);
+            if(velocity.y < maxFallVel)
+                velocity.y = maxFallVel;
+        }
+        // Handel movement
+        if (!isGrounded)
+            velocity = Accelerate(wishDir, velocity, airAcc, maxAirVel);
+        else
+            velocity = move(wishDir, velocity, groundAcc, maxGroundVel, friction);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask); // Makes a sphere at groundCheck to detect ground collisions
+
+        // Handle Jump
+        if (jumpBufferTime > 0 && isGrounded)
+        {
+            velocity.y = jumpForce;
+            jumpBufferTime = 0;
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+                velocity.y = jumpForce;
+            else
                 jumpBufferTime = jumpBuffer;
         }
 
-        Vector3 wishDir = Vector3.Normalize(currentDir);
-
-        // Handel movement
-        if (isGrounded)
-            velocity = move(wishDir, velocity, groundAcc, maxGroundVel, friction);
-        else
-            velocity = move(wishDir, velocity, airAcc, maxAirVel, airResistance);
+        controller.Move(velocity * Time.fixedDeltaTime);
         
-        // Add gravity
-        velocity.y += gravity * Time.deltaTime;
-        if(velocity.y < maxFallVel) // Set player terminal velocity
-            velocity.y = maxFallVel;
-
-        controller.Move(velocity * Time.deltaTime);
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask); // Makes a sphere at groundCheck to detect ground collisions
-
-
         // Decrease timers
-        if(jumpBufferTime >= 0)
+        if (jumpBufferTime > 0)
             jumpBufferTime -= Time.deltaTime;
     }
 
     private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVel, float accel, float maxVel)
     {
         float projVel = Vector3.Dot(prevVel, accelDir);
-        float accVel = accel * Time.deltaTime;
+        float accVel = accel * Time.fixedDeltaTime;
 
-        if(projVel + accVel > maxVel)
+        if (projVel + accVel > maxVel)
             accVel = maxVel - projVel;
 
         return prevVel + accelDir * accVel;
@@ -86,10 +91,10 @@ public class AltCharecterMovement : MonoBehaviour
     private Vector3 move(Vector3 accelDir, Vector3 prevVel, float acc, float maxAcc, float resistance)
     {
         float speed = prevVel.magnitude;
-        speed = Mathf.Round(speed * 1)/1; //  Prevents sliding and jittering when stopping or changing direction
-        if(speed != 0)
+        speed = Mathf.Round(speed * 10) / 10; //  Prevents sliding and jittering when stopping or changing direction
+        float drop = speed * resistance * Time.fixedDeltaTime;
+        if (speed != 0)
         {
-            float drop = speed * resistance * Time.deltaTime;
             prevVel *= Mathf.Max(speed - drop, 0) / speed;
         }
         return Accelerate(accelDir, prevVel, acc, maxAcc);
