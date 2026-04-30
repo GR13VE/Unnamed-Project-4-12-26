322e3cb3d8c0f03bb624aa5907618a1acdc92448
Rubber Ducky Software Development <78836929+RDSD-Dev@users.noreply.github.com>
2026-04-30 03:46:16 -0600

    Added new enemy

--- Assets/Scripts/flyingEnemy.cs
+++ Assets/Scripts/flyingEnemy.cs
@@ -5,12 +5,16 @@ public class flyingEnemy : MonoBehaviour
     [Header("Enemy")]
     public float speed = 10f;
     public float maxAxx = 12f;
-    public float turnRadius = 4f;
+    public float turnRadius = 26f;
+    public float correctionForce = 1.2f;
+
     public int damage = 1;
 
     [Header("Setup")]
     public Transform enemy;
     public CharacterController controller;
+
+    float angleToPlayer;
     Vector3 wishDirection;
 
     GameObject player;
@@ -18,43 +22,44 @@ public class flyingEnemy : MonoBehaviour
     void Start()
     {
         this.player = GameObject.FindWithTag("Player");
-        wishDirection = new Vector3();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void FixedUpdate() 
     {
+        /*
+            I need to change this to move toward target full speed unless target goes outside of in front of self
+            if target no longer in sight then we need to slow down in order to re orient self.
+        */
 
         this.player = GameObject.FindWithTag("Player"); // Current player position
 
         wishDirection = player.transform.position - enemy.position;
+        angleToPlayer = Vector3.Angle(wishDirection, enemy.forward);
 
-        controller.Move(move(Vector3.Normalize(wishDirection), controller.velocity, speed, maxAxx, turnRadius) * Time.fixedDeltaTime);
+        controller.Move(Accelerate(Vector3.Normalize(wishDirection), controller.velocity, speed, maxAxx) * Time.fixedDeltaTime);
         enemy.rotation = Quaternion.LookRotation(controller.velocity);
 
     }
 
         private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVel, float accel, float maxVel)
     {
-        float speed = prevVel.magnitude;
-        float drop = speed * turnRadius * Time.fixedDeltaTime;
-        float projVel = Vector3.Dot(prevVel * (speed -drop / speed), accelDir);
+        if(angleToPlayer > turnRadius)
+        {
+            float speed = prevVel.magnitude;
+            float drop = correctionForce * speed * Time.fixedDeltaTime;
+            if(speed != 0)
+                prevVel *= Mathf.Max(speed - drop, 0) / speed;
+            accel *= correctionForce;
+        }
+            
+        
+        float projVel = Vector3.Dot(prevVel, accelDir);
         float accVel = accel * Time.fixedDeltaTime;
 
         if (projVel + accVel > maxVel)
             accVel = maxVel - projVel;
 
         return prevVel + accelDir * accVel;
-    }
-        private Vector3 move(Vector3 accelDir, Vector3 prevVel, float acc, float maxAcc, float resistance)
-    {
-        float speed = prevVel.magnitude;
-        // Calculate how much friction to be applied this frame
-        float drop = speed * resistance * Time.fixedDeltaTime; // If we are not giving an input apply stop force
-        
-        if (speed != 0) // Avoid divide by zero
-            prevVel *= Mathf.Max(speed - drop, 0) / speed;
-
-        return Accelerate(accelDir, prevVel, acc, maxAcc);
     }
 }
